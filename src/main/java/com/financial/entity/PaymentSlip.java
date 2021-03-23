9003c070128678c7d1b13e407cf441c7b1821540
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-03-23 01:52:09 -0300

    Entidade - PaymentSlip

---
+++ src/main/java/com/financial/entity/PaymentSlip.java
@@ -0,0 +1,48 @@
+package com.financial.entity;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.financial.entity.enums.StagePayment;
+
+@Entity
+public class PaymentSlip extends Payment{
+	private static final long serialVersionUID = 1L;
+
+	@JsonFormat(pattern="dd/MM/yyyy")
+	private OffsetDateTime dueDate = OffsetDateTime.now();
+
+	@JsonFormat(pattern="dd/MM/yyyy")
+	private OffsetDateTime payDay = OffsetDateTime.now();
+
+	public PaymentSlip() {
+	}
+
+	public PaymentSlip(Long id, StagePayment stage, Request request, OffsetDateTime dueDate, OffsetDateTime payDay) {
+		super(id, stage, request);
+		this.dueDate = dueDate;
+		this.payDay = payDay;
+	}
+
+	public OffsetDateTime getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(OffsetDateTime dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public OffsetDateTime getPayDay() {
+		return payDay;
+	}
+
+	public void setPayDay(OffsetDateTime payDay) {
+		this.payDay = payDay;
+	}
+	
+	
+	
+	
+}
