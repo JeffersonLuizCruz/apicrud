43de5ca6dda3636a9ed2ec3632d20760f415c6f6
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-05-02 01:10:05 -0300

    Profile de desenvolvimento

---
+++ src/main/java/com/financial/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.financial.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.financial.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired private DBService dbService;
+	
+	/**
+	 * Método responsável por instanciar no bando de dados no Profile de teste.
+	 * */
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
