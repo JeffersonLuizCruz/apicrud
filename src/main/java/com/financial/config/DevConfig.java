5166b75f88a1f6eb85f9d580bdd29ca1b5beebf3
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-05-02 19:02:41 -0300

    teste1

--- src/main/java/com/financial/config/DevConfig.java
+++ src/main/java/com/financial/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.financial.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,13 +14,19 @@ import com.financial.service.DBService;
 @Profile("dev")
 public class DevConfig {
 	
-	@Autowired private DBService dbService;
+	@Autowired
+	private DBService dbService;
 	
-	/**
-	 * Método responsável por instanciar no bando de dados no Profile de teste.
-	 * */
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
