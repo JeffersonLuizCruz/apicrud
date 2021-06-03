5a7817312b3b90b69242089f8be1884f35a1bca5
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2021-06-03 19:18:49 -0300

    Implementado Autorização

--- src/main/java/com/financial/config/SecurityConfig.java
+++ src/main/java/com/financial/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.financial.security.JWTAuthenticationFilter;
+import com.financial.security.JWTAuthorizationFilter;
 import com.financial.security.JWTUtil;
 
 
@@ -62,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		//http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
